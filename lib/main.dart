d88819ec97a71cb27086d0eb942de53d79e98908
Sumit <bharangarsinghsumit@gmail.com>
2026-08-24 11:26:11 +0530

    pushed

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: HomePage(),
     );
   }
-}
+} 
